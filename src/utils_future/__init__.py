09016d5bfe06ae7b685fe4d72e175737cf763fdd
nuuuwan <nuuuwan@gmail.com>
2023-12-21 17:40:24 +0530

    lint errors

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -1,5 +1,11 @@
+from utils import Log
+
+log = Log('utils_future')
+
+
 def parse_float(x) -> float:
     try:
         return float(str(x))
     except Exception as e:
+        log.error(f'parse_float({x}) failed: {e}')
         return None
