1edab614bd80a5c7ee3df9d12522a0a9596e92fe
Nuwan I. Senaratna <nuuuwan@gmail.com>
2023-12-21 12:32:14 +0530

    Update pipeline.py

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,10 +1,10 @@
 from lk_food.scrapers.ReadMe import ReadMe
-
+from lk_food.scrapers.StoreFactory import StoreFactory
 
 def main():
-    # for store_class in StoreFactory.list_all():
-    #     store = store_class()
-    #     store.scrape()
+    for store_class in StoreFactory.list_all():
+        store = store_class()
+        store.scrape()
 
     ReadMe().write()
 
