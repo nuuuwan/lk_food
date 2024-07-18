0f7669be4cf27f4e95466958eaf77789db26d04e
nuuuwan <nuuuwan@gmail.com>
2024-07-18 15:16:49 +0530

    More bug fixes

--- src/utils_future/Float.py
+++ src/utils_future/Float.py
@@ -1,7 +1,11 @@
+from utils import Log 
+log = Log('Float')
+
 class Float:
     @staticmethod
     def parse(x):
         try:
             return float(x)
-        except ValueError:
+        except Exception as e:
+            log.error(f"Float.parse({x}) failed: {e}")
             return None
